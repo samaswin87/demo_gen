0205d3e4f11b50cf37e869832d8f58a157f7d155
samaswin87 <aswin.r312@outlook.com>
2020-09-22 20:10:14 +0530

    added tailwind template

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+let environment = {
+  plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+};
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        '.app/**/*.html.erb',
+        '.app/helpers/**/*.rb',
+        '.app/javascript/**/*.js',
+        '.app/javascript/**/*.vue',
+        '.app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9:/]+/g) || []
+    })
+  )
+}
+module.exports = environment;
